94f8dd008544b1449041831dcce30f7b0c45bb0f
Wiktor <wiktor.muraszko@wp.pl>
2019-06-08 22:23:21 +0200

    T2+T3 added

---
+++ src/main/java/KSRZadanie2/Quality/DegreeOfAppropriatness.java
@@ -0,0 +1,16 @@
+package KSRZadanie2.Quality;
+
+import KSRZadanie2.Model.Summary.FirstTypeSummary;
+import KSRZadanie2.Model.Summary.SecondTypeSummary;
+
+public class DegreeOfAppropriatness implements IDegree {
+    @Override
+    public Double Calc(FirstTypeSummary firstTypeSummary) {
+        return null;
+    }
+
+    @Override
+    public Double Calc(SecondTypeSummary secondTypeSummary) {
+        return null;
+    }
+}
--- src/main/java/KSRZadanie2/Quality/DegreeOfPrecision.java
+++ src/main/java/KSRZadanie2/Quality/DegreeOfPrecision.java
@@ -1,4 +1,84 @@
 package KSRZadanie2.Quality;
 
-public class DegreeOfPrecision {
+import KSRZadanie2.Model.Summary.FirstTypeSummary;
+import KSRZadanie2.Model.Summary.SecondTypeSummary;
+
+public class DegreeOfPrecision implements IDegree {
+    @Override
+    public Double Calc(FirstTypeSummary firstTypeSummary) {
+        if(firstTypeSummary.Operation.equals("NONE")) {
+            return 1 - DegreeOfFuzzines(firstTypeSummary);
+        }else {
+            return 1 - Math.sqrt(DegreeOfFuzzines(firstTypeSummary));
+        }
+    }
+
+    @Override
+    public Double Calc(SecondTypeSummary secondTypeSummary) {
+        if(secondTypeSummary.Operation.equals("NONE")) {
+            return 1 - Math.sqrt(DegreeOfFuzzines(secondTypeSummary));
+        }else {
+            return 1 - Math.pow(DegreeOfFuzzines(secondTypeSummary),1f/3f);
+        }
+    }
+    private Double DegreeOfFuzzines(FirstTypeSummary firstTypeSummary){
+        int counter =0;
+        double fuzzines;
+        if (firstTypeSummary.Operation.equals("NONE")) {
+            for(Double value : firstTypeSummary.ValuesForSummarizer1){
+                if(firstTypeSummary.Summarizer1.fuzzySet.MemberFuncResult(value)!=0){
+                    counter ++;
+                }
+            }
+            fuzzines = (double) counter/(double) firstTypeSummary.ValuesForSummarizer1.size();
+        } else {
+            for(Double value : firstTypeSummary.ValuesForSummarizer1){
+                if(firstTypeSummary.Summarizer1.fuzzySet.MemberFuncResult(value)!=0){
+                    counter ++;
+                }
+            }
+            fuzzines = (double) counter/(double) firstTypeSummary.ValuesForSummarizer1.size();
+            counter =0 ;
+            for(Double value : firstTypeSummary.ValuesForSummarizer2){
+                if(firstTypeSummary.Summarizer2.fuzzySet.MemberFuncResult(value)!=0){
+                    counter ++;
+                }
+            }
+            fuzzines *= (double) counter/(double) firstTypeSummary.ValuesForSummarizer2.size();
+        }
+        return fuzzines;
+    }
+    private Double DegreeOfFuzzines(SecondTypeSummary secondTypeSummary){
+        int counter =0;
+        double fuzzines;
+        if (secondTypeSummary.Operation.equals("NONE")) {
+            for(Double value : secondTypeSummary.ValuesForSummarizer1){
+                if(secondTypeSummary.Summarizer1.fuzzySet.MemberFuncResult(value)!=0){
+                    counter ++;
+                }
+            }
+            fuzzines = (double) counter/(double) secondTypeSummary.ValuesForSummarizer1.size();
+        } else {
+            for(Double value : secondTypeSummary.ValuesForSummarizer1){
+                if(secondTypeSummary.Summarizer1.fuzzySet.MemberFuncResult(value)!=0){
+                    counter ++;
+                }
+            }
+            fuzzines = (double) counter/(double) secondTypeSummary.ValuesForSummarizer1.size();
+            counter =0 ;
+            for(Double value : secondTypeSummary.ValuesForSummarizer2){
+                if(secondTypeSummary.Summarizer2.fuzzySet.MemberFuncResult(value)!=0){
+                    counter ++;
+                }
+            }
+            fuzzines *= (double) counter/(double) secondTypeSummary.ValuesForSummarizer2.size();
+        }
+        for(Double value : secondTypeSummary.ValuesForQualifier){
+            if(secondTypeSummary.Qualifier.fuzzySet.MemberFuncResult(value)!=0){
+                counter ++;
+            }
+        }
+        fuzzines *= (double) counter/(double) secondTypeSummary.ValuesForQualifier.size();
+        return fuzzines;
+    }
 }
